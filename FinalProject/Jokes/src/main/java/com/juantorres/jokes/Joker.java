5b89e68f47a3a7577eda8f325b246bbb53874fd1
Juan Torres <juanetf@gmail.com>
2018-03-04 14:07:47 -0400

    Steps 0 and 1 completed

---
+++ FinalProject/Jokes/src/main/java/com/juantorres/jokes/Joker.java
@@ -0,0 +1,6 @@
+package com.juantorres.jokes;
+
+public class Joker {
+    private static final String CONSTANT_JOKE = "Joke Random!";
+    public static String getRandomJoke(){ return CONSTANT_JOKE;}
+}
